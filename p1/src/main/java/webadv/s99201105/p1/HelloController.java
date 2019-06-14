b40bbabae87389f9792d18d982ba59cfc83e304f
S16202222 <794657699@qq.com>
2019-06-14 22:08:39 +0800

    p1

---
+++ p1/src/main/java/webadv/s99201105/p1/HelloController.java
@@ -0,0 +1,14 @@
+package webadv.s99201105.p1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "welcome";
+    }
+
+}
