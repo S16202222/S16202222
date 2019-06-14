a527b1ba821012319d0538e02f3b6f0eb1b37b77
S16202222 <794657699@qq.com>
2019-06-14 22:19:36 +0800

    lab2

---
+++ lab2/src/main/java/webadv/s99201105/rest/Task.java
@@ -0,0 +1,21 @@
+package webadv.s99201105.rest;
+
+public class Task {
+	String str;
+	int id;
+	public Task(int id,String str) {
+		this.id=id;
+		this.str=str;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public static Object get(int i) {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+}
