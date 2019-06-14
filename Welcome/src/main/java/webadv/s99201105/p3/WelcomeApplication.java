3a6ea511cb12dc9fa432e5a7d5f77f4988f35471
S16202222 <794657699@qq.com>
2019-06-14 21:04:53 +0800

    Welcome

---
+++ Welcome/src/main/java/webadv/s99201105/p3/WelcomeApplication.java
@@ -0,0 +1,13 @@
+package webadv.s99201105.p3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WelcomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WelcomeApplication.class, args);
+	}
+
+}
